aadb05a0c14f880eff14b4cf0e6c561c0a40ebe7
titanknis <titanknis@gmail.com>
2025-10-14 13:31:37 +0100

    😄 👍

--- Semestre1/Java/TPs/TP3/ex7/TestCompteBancaire.java
+++ Semestre1/Java/TPs/TP3/ex7/TestCompteBancaire.java
@@ -2,7 +2,7 @@ public class TestCompteBancaire {
   public static void main(String[] args) {
     CompteBancaire CB[] = new CompteBancaire[100];
     CB[0] = new CompteBancaire(0, "Ali Laouti", 1000000000);
-    CB[1] = new CompteBancaire(1, "rayen", -1000000000);
+    CB[1] = new CompteBancaire(1, "rayen jaidi", 1000000000);
     for (int i = 2; i < CB.length; i++) {
       CB[i] = new CompteBancaire(i, "knis" + i, 10 * i);
       CB[i].deposer(1000);
